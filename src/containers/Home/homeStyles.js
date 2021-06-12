2dbd9e2f090c8689d6698b345ac745a11909526b
Bautista Cutraro <bautistacutraro@gmail.com>
2021-06-12 15:46:08 -0300

    initial form styles

---
+++ src/containers/Home/homeStyles.js
@@ -0,0 +1,24 @@
+import styled from './styled-components';
+
+export const Container = styled.div``;
+
+export const Form = styled.form``;
+
+export const FormSubcontainer = styled.fieldset``;
+
+export const Title = styled.h1``;
+
+export const ProductsList = styled.ul``;
+
+export const ProductItem = styled.li``;
+
+export const ProductItemTitle = styled.padding``;
+export const ProductItemCount = styled.input``;
+
+export const InputContainer = styled.label``;
+
+export const Input = styled.input``;
+
+export const InputError = styled.p``;
+
+export const Button = styled.button``;
