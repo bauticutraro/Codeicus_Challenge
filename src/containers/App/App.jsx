67c9ea74d692181d35545978ad4aed8ac277401e
Bautista Cutraro <bautistacutraro@gmail.com>
2021-06-12 15:41:13 -0300

    initial config

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GlobalStyles from '../../globalStyles';
+import Home from '../Home/Home';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+
+      <Home />
+    </>
+  );
+};
+
+export default App;
